872139fa395d0ddea1dd8f5a5c5cf5e71b061d4c
christianemmanuel <ian34concepcion@gmail.com>
2021-07-11 20:18:20 +0800

    done for some pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,55 +1,69 @@
-$("#nav ul li a[href^='#']").on('click', function(e) {
+$(function() {
 
-  // prevent default anchor click behavior
-  e.preventDefault();
+  $("#nav ul li a[href^='#']").on('click', function(e) {
 
-  // store hash
-  var hash = this.hash;
+    // prevent default anchor click behavior
+    e.preventDefault();
 
-  // animate
-  $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 1000, function(){
+    // store hash
+    var hash = this.hash;
 
-      // when done, add hash to url
-      // (default click behaviour)
-      window.location.hash = hash;
-    });
+    // animate
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
 
-});
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+      });
 
-$("#nav div div a[href^='#']").on('click', function(e) {
+  });
 
-  // prevent default anchor click behavior
-  e.preventDefault();
+  $("#nav div div a[href^='#']").on('click', function(e) {
 
-  // store hash
-  var hash = this.hash;
+    // prevent default anchor click behavior
+    e.preventDefault();
 
-  // animate
-  $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 1000, function(){
+    // store hash
+    var hash = this.hash;
 
-      // when done, add hash to url
-      // (default click behaviour)
-      window.location.hash = hash;
-    });
+    // animate
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
 
-});
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+      });
 
-$('#toggle_menu').click(function() {
-  $('#nav').slideToggle();
-});
-$('#nav a').click(function() {
-  $('#nav').removeAttr('style')
-});
+  });
 
+  $('#toggle_menu').click(function() {
+    $('#nav').slideToggle();
+  });
+  $('#nav a').click(function() {
+    $('#nav').removeAttr('style')
+  });
 
-$(".species_three1, .species_three2").mouseenter(function() {
-  $(".species_three1").addClass('show');
-  $(".species_three2").addClass('show');
-}).mouseleave(function() {
-  $(".species_three1").removeClass('show');
-  $(".species_three2").removeClass('show');
+  $(".species_three1, .species_three2").mouseenter(function() {
+    $(".species_three1").addClass('show');
+    $(".species_three2").addClass('show');
+  }).mouseleave(function() {
+    $(".species_three1").removeClass('show');
+    $(".species_three2").removeClass('show');
+  });
+
+  $(document).tooltip({
+    position: { my: "center", at: "bottom" }
+  });
+
+  $('.slider-list').slick();
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
 });
